65705b4f05070969e298f6ff650436a4cbdbdc73
kareemrafei <kareemrafei26@gmail.com>
2025-07-26 22:11:46 +0300

    Create SalonCard.tsx

---
+++ src/components/ui/SalonCard.tsx
@@ -0,0 +1,11 @@
+const SalonCard = () => {
+  return (
+    <div className="mt-6 border rounded-lg shadow-md p-4">
+      <h2 className="text-xl font-bold mb-2">Reda's Barbershop</h2>
+      <p className="text-gray-600">Tripoli - Open 10am to 9pm</p>
+      <button className="mt-3 px-4 py-2 bg-black text-white rounded">Book Now</button>
+    </div>
+  );
+};
+
+export default SalonCard;
