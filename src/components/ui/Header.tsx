4f3b6af88aead4bfd636c83e665a27f25fca0126
kareemrafei <kareemrafei26@gmail.com>
2025-07-26 22:10:36 +0300

    Create Header.tsx

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full py-4 px-6 bg-black text-white text-2xl font-semibold">
+      GlowUp 💈 Booking App
+    </header>
+  );
+};
+
+export default Header;
