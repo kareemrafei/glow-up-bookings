19066201a74ee7068b1b0edf777ec3ec3751035a
kareemrafei <kareemrafei26@gmail.com>
2025-07-26 22:25:59 +0300

    Update Index.tsx 3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,19 @@
 // Update this page (the content is just a fallback if you fail to update the page)
-import Header from "../components/Header";
-
 const Index = () => {
   return (
     <div className="min-h-screen bg-white p-6">
-      <Header />
+      {/* <Header /> */}
+      {/* <div className="my-6">
+        <CategoryCard />
+        <SalonCard />
+      </div> */}
+      <h1>Step 1: No components. Just this.</h1>
     </div>
   );
 };
 
 export default Index;
+
 
 
 
